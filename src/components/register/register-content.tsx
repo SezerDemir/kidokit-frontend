00e6cecd9b45768d7f5d54df67c9fe36b4859d87
Sezer Demir <sezer_d@outlook.com>
2023-02-22 10:57:43 +0300

    register page error alert updated

--- src/components/register/register-content.tsx
+++ src/components/register/register-content.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent, ChangeEvent } from "react"
+import React, { useState, FormEvent, ChangeEvent, useEffect } from "react"
 import styles from "./register-style.module.css"
 import warningLogo from "../../images/warningSign.png"
 import axios from "axios"
@@ -23,7 +23,6 @@ export function RegisterContent(){
     const [passwordDigitCheck, setPasswordDigitCheck] = useState<boolean>(false);
     const [passwordSpecialCheck, setPasswordSpecialCheck] = useState<boolean>(false);
     const [passwrodUpperCaseCheck, setPasswrodUpperCaseCheck] = useState<boolean>(false);
-    const [repeatPasswordCheck, setRepeatPasswordCheck] = useState<boolean>(false);
 
     const [fullName, setFullName] = useState<string>("");
     const [password, setPassword] = useState<string>("");
@@ -31,6 +30,33 @@ export function RegisterContent(){
     const [email, setEmail] = useState<string>("");
     const [childName, setChildName] = useState<string>("");
     const [childBirthday, setChildBirthday] = useState<string>("");
+
+    const[nameInputStyle, setNameInputStyle] = useState<string>("");
+    const[emailInputStyle, setEmailInputStyle] = useState<string>("");
+    const[passwordInputStyle, setPasswordInputStyle] = useState<string>("");
+    const[repeatPasswordInputStyle, setRepeatPasswordInputStyle] = useState<string>("");
+    const[childNameInputStyle, setChildNameInputStyle] = useState<string>("");
+    const[childBirthdayInputStyle, setChildBirthdayInputStyle] = useState<string>("");
+    const[childGenderMaleInputStyle, setChildGenderMaleInputStyle] = useState<string>("");
+    const[childGenderFemaleInputStyle, setChildGenderFemaleInputStyle] = useState<string>("");
+    const[agreementInputStyle, setAgreementInputStyle] = useState<string>("");
+
+    // error effects for input fields
+    useEffect( () => {
+        const timer = setTimeout(() => {
+            setNameInputStyle("");
+            setEmailInputStyle("");
+            setPasswordInputStyle("");
+            setRepeatPasswordInputStyle("");
+            setChildNameInputStyle("");
+            setChildBirthdayInputStyle("");
+            setChildGenderMaleInputStyle("");
+            setChildGenderFemaleInputStyle("");
+            setAgreementInputStyle("");
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, [nameInputStyle, emailInputStyle, passwordInputStyle, repeatPasswordInputStyle,
+        childNameInputStyle, childBirthdayInputStyle, childGenderMaleInputStyle, childGenderFemaleInputStyle, agreementInputStyle]);
 
     enum Gender{
         female = 0, male = 1, none = 2
@@ -128,7 +154,7 @@ export function RegisterContent(){
         const childBirthDayCheck: boolean = (childBirthday !== "") ? true : false;
         const childGenderCheck: boolean = (Gender.none !== childGender) ? true : false;
         const agreement: boolean = (document.getElementById("agreement-checkbox") as HTMLInputElement).checked;
-        const repeatCheck: boolean = (password===repeatPassword ? true:false);
+        const repeatCheck: boolean = (password===repeatPassword && repeatPassword !== "" ? true:false);
 
         console.log(fullName + " " + email + " " + password + " " + repeatPassword);
         console.log(nameCheck + " " + emailCheck + " " + passwordCheck + " " + repeatCheck);
@@ -144,15 +170,27 @@ export function RegisterContent(){
                     "childGender" : childGender
                 }
             );
-            axios.post(apiRegisterAdress, jsonString, {headers: {'Content-Type': 'application/json'}})
-                .then(response => {
-                    console.log(response)
-                });
-                alert("Successfully Registered");
-                window.location.replace("/");
+            registerRequest(apiRegisterAdress, jsonString);
+            
+            alert("Successfully Registered");
+            //window.location.replace("/");
         }
         else{
-            const errorMessage: string = 
+            if(!nameCheck) setNameInputStyle("input-warning-color-switch");
+            if(!emailCheck) setEmailInputStyle("input-warning-color-switch");
+            if(!passwordCheck) setPasswordInputStyle("input-warning-color-switch");
+            if(!repeatCheck) setRepeatPasswordInputStyle("input-warning-color-switch");
+            if(!childNameCheck) setChildNameInputStyle("input-warning-color-switch");
+            if(!childBirthDayCheck) setChildBirthdayInputStyle("input-warning-color-switch");
+            if(!childGenderCheck) {
+                setChildGenderMaleInputStyle("input-warning-color-switch");
+                setChildGenderFemaleInputStyle("input-warning-color-switch");
+            }
+            if(!agreement){
+                setAgreementInputStyle("input-warning-color-switch");
+                console.log("here");
+            } 
+            /* const errorMessage: string = 
                 (nameCheck ? "" : fullNameErrorMessage + "\n") + 
                 (emailCheck ? "" : emailErrorMessage + "\n") + 
                 (passwordCheck ? "" : passwordErrorMessage + "\n") + 
@@ -161,7 +199,7 @@ export function RegisterContent(){
                 (childBirthDayCheck ? "" : childBirthdayErrorMessage + "\n") + 
                 (childGenderCheck ? "" : childGenderErrorMessage + "\n") + 
                 (agreement ? "" : agreementErrorMessage);
-            alert(errorMessage);
+            alert(errorMessage); */
         }
     }
     
@@ -169,17 +207,17 @@ export function RegisterContent(){
         <form className={styles["register-content-container"]} onSubmit={handleSubmit}>
             <div className={styles["name-cell"]}>
                 <h1>Adınız ve Soyadınız</h1>
-                <input type={"text"} id="input-name" onChange={handleInputChange}/>
+                <input type={"text"} id="input-name" className={styles[nameInputStyle]} onChange={handleInputChange}/>
             </div>
 
             <div className={styles["email-cell"]}>
                 <h1>Eposta adresinizi yazınız</h1>
-                <input type={"text"} id="input-email" onChange={handleInputChange}/>
+                <input type={"text"} id="input-email" className={styles[emailInputStyle]} onChange={handleInputChange}/>
             </div>
             
             <div className={styles["password-cell"]}>
                 <h1>Bir şifre belirleyiniz</h1>
-                <input type={passwordShown ? "text" : "password"} onChange={validatePassword} id="input-password"/>
+                <input type={passwordShown ? "text" : "password"} className={styles[passwordInputStyle]} onChange={validatePassword} id="input-password"/>
                 <button type="button" onClick={() => {passwordShown ? setPasswordShown(false) : setPasswordShown(true)}}></button>
    
                 <ul>
@@ -192,7 +230,7 @@ export function RegisterContent(){
                 
             <div className={styles["repeat-password-cell"]}>
                 <h1>Şifreyi tekrar giriniz</h1>
-                <input type={repeatPasswordShown ? "text" : "password"} id="input-repeat-password" 
+                <input type={repeatPasswordShown ? "text" : "password"} className={styles[repeatPasswordInputStyle]} id="input-repeat-password" 
                     onChange={handleInputChange}
                     onPaste={() => {console.log(navigator.clipboard.readText())}}
                 />
@@ -201,23 +239,23 @@ export function RegisterContent(){
 
             <div className={styles["kid-name-cell"]}>
                 <h1>Çocuğun ismi?</h1>
-                <input type="text" id="input-kid-name" onChange={handleInputChange}/>
+                <input type="text" id="input-kid-name" className={styles[childNameInputStyle]} onChange={handleInputChange}/>
             </div>
 
             <div className={styles["kid-birthday-cell"]}>
                 <h1>Çocuğun Doğum Tarihi?</h1>
-                <input type={"date"} id="input-date" onChange={handleInputChange} />
+                <input type={"date"} id="input-date" className={styles[childBirthdayInputStyle]} onChange={handleInputChange} />
             </div>
 
             <div className={styles["kid-gender-cell"]}>
                 <h1>Çocuğunuzun cinsiyeti</h1>
-                <button type="button" className={styles["button-male"]} id="male-button" onClick={() => 
+                <button type="button" className={styles["button-male"] + " " + styles[childGenderMaleInputStyle]} id="male-button" onClick={() => 
                     {
                         setChildGender(Gender.male);
                         changeButtonColor(document.getElementById("male-button"), document.getElementById("female-button"));
                     }
                    }>Erkek</button>
-                <button type="button" className={styles["button-female"]} id="female-button" onClick={() => 
+                <button type="button" className={styles["button-female"] + " " + styles[childGenderFemaleInputStyle]} id="female-button" onClick={() => 
                     {
                         setChildGender(Gender.female);
                         changeButtonColor(document.getElementById("female-button"), document.getElementById("male-button"));
@@ -232,7 +270,7 @@ export function RegisterContent(){
 
             <div className={styles["agreement-cell"]}>
                 <div className={styles["agreement-item"]}>
-                    <input type="checkbox" id="agreement-checkbox"></input>
+                    <input type="checkbox" id="agreement-checkbox" className={styles[agreementInputStyle]}></input>
                     <p><a href="">Kullanım Koşullarını ve Gizlilik Sözleşmesini</a> okudum, kabul ediyorum.</p>
                 </div>
                 
@@ -244,4 +282,11 @@ export function RegisterContent(){
         </form>
         
     );
+}
+
+async function registerRequest(api: string, requestBody: string){
+    await axios.post(api, requestBody, {headers: {'Content-Type': 'application/json'}})
+                .then(response => {
+                    console.log(response)
+                });
 }
